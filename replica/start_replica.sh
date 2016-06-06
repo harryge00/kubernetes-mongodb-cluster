7e7d449dc49d7f6d29080e92cbce2dd5d8674d2c
haoyuan <harryge00@gmail.com>
2016-06-06 18:56:18 +0800

    tmp

--- replica/start_replica.sh
+++ replica/start_replica.sh
@@ -1,9 +1,27 @@
-mongod --replSet $SET_NAME &
+#! /bin/bash
+# start mongoDB
+mongod --replSet $mongo_replica_set_name &
+
+# create the mongo-shell file: build_mongo_replication.js
+echo $mongo_node_name
+content="rs.initiate({_id:"
+content="$content \"$mongo_replica_set_name\", members: [{_id:0, host:\"${mongo_node_name}0\" }"
+for i in $(eval echo {1..$mongo_nodes_number}); do
+	echo $i
+	mongo_members="${mongo_members}, { _id:$i, host:\"$mongo_node_name$i\" }"
+done;
+content="$content $mongo_members]});"
+echo $content > build_mongo_replication.js
+echo "build_mongo_replication.js created"
+cat ./build_mongo_replication.js
+
+# Wait until mongoDB initialized
 until nc -z localhost 27017
 do
-    echo "waiting"
+    echo "MongoDB initializing"
     sleep 1
 done
-echo "done"
-mongo < /build_replica.js
+echo "MongoDB initialized"
+# Start replica set
+mongo < ./build_mongo_replication.js
 
